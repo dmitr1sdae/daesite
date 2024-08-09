3e71ebaa97ad9caee48eecb129001ed1de5b4b85
Dmitriy Chukhno <me@dmitr1sdae.com>
2024-08-09 18:47:17 +0200

    feat(accounts): improve 404 page content and styling
    - Enhanced `not-found.scss` by adding styles for `.content` and `.text` to structure and style the 404 page message.
    - Updated `not-found.tsx` with new content, including a more descriptive heading, subheadings, and navigation links to improve user guidance.
    - Integrated `AppLink` component for navigation within the 404 page, providing links to the homepage and search page.
    - Adjusted the page title in the metadata to better reflect the 404 status.

--- applications/accounts/app/not-found.tsx
+++ applications/accounts/app/not-found.tsx
@@ -1,30 +1,43 @@
-import {EpicTitle} from "~/components/EpicTitle";
 import {Metadata} from "next/types";
-import {StarsBackground} from "~/components/StarsBackground";
 import Image from "next/image";
 import power from "@daesite/styles/assets/img/brand/power-looks-at-the-sky.webp";
-import "./not-found.scss";
+import {StarsBackground} from "~/components/StarsBackground";
 import {Header} from "~/components/Header";
 import {Fullscreen} from "~/containers/Fullscreen";
+import {AppLink} from "@daesite/components";
+import "./not-found.scss";
 
 export const metadata: Metadata = {
-  title: "Page Not Found - dadaya",
+  title: "404 - Lost Among the Stars",
 };
 
 const NotExists = () => {
   return (
-    <>
-      <Fullscreen className="overflow-hidden" background={StarsBackground}>
-        <Header />
-        <EpicTitle className="flex h-full">NOT EXISTS</EpicTitle>
-        <Image
-          className="power"
-          src={power}
-          alt="power looks at the sky 💜"
-          priority={true}
-        />
-      </Fullscreen>
-    </>
+    <Fullscreen className="overflow-hidden" background={StarsBackground}>
+      <Header />
+      <main className="content">
+        <section className="flex flex-column gap-6">
+          <h1 className="text-4xl text-black text-uppercase">
+            Oops, you're lost among the stars!
+          </h1>
+          <div>
+            <h3 className="text-xl text-semibold">
+              The page you seek has vanished like a falling star.
+            </h3>
+            <h3 className="text-xl text-semibold">
+              Return to the <AppLink to="/">homepage</AppLink> or use the{" "}
+              <AppLink to="/search">search</AppLink> to find your way.
+            </h3>
+          </div>
+        </section>
+      </main>
+      <Image
+        className="power"
+        src={power}
+        alt="power looks at the sky 💜"
+        priority
+      />
+    </Fullscreen>
   );
 };
 
